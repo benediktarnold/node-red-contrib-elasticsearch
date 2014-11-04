4f528d4995060b574c0833de288aa57bdded9529
Benedikt Arnold <benedikt.arnold@gmx.net>
2014-11-04 16:49:02 +0100

    build directory in git for usage with npm

---
+++ build/elasticsearch.js
@@ -0,0 +1,34 @@
+var elasticsearch;
+
+elasticsearch = require('elasticsearch');
+
+module.exports = function(RED) {
+  var ElasticsearchNode;
+  ElasticsearchNode = function(config) {
+    var node;
+    RED.nodes.createNode(this, config);
+    node = this;
+    return node.on("input", function(msg) {
+      var es;
+      es = new elasticsearch.Client({
+        host: config.host + ":" + config.port,
+        apiVersion: '1.3',
+        log: 'trace'
+      });
+      console.log(config);
+      return es.create({
+        index: config.index,
+        type: config.documenttype,
+        body: msg
+      }, function(error, response) {
+        if (error) {
+          node.error(error, response);
+        } else {
+          node.warn("written to elasticsearch");
+        }
+        return es.close;
+      });
+    });
+  };
+  return RED.nodes.registerType("elasticsearch", ElasticsearchNode);
+};
